6322d0771e889a00c7d327054bde9560772bac87
Jojo Zhang <jojozhang0327@gmail.com>
2020-07-08 16:00:24 -0400

    complete  lookup table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -2,6 +2,8 @@
 import math
 import random
 
+lookup_table = {}
+
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -18,6 +20,20 @@ def slowfun(x, y):
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    if (x, y) in lookup_table:
+        return lookup_table[(x, y)]
+    v = math.pow(x, y)
+    v = math.factorial(v)
+    v //= (x + y)
+    v %= 982451653
+    lookup_table[(x, y)] = v
+    return v
+
+
+# for i in range(50):
+#     x = random.randrange(2, 14)
+#     y = random.randrange(3, 6)
+#     print(f'{i}: {x},{y}: {slowfun(x, y)}')
 
 
 # Do not modify below this line!
