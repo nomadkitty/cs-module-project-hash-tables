b71b55d164042bf8d3c079369b9a42e6d47c35e4
Jojo Zhang <jojozhang0327@gmail.com>
2020-07-09 23:22:39 -0400

    update markov

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 # Read in all the words in one go
 with open("input.txt") as f:
@@ -6,8 +7,31 @@ with open("input.txt") as f:
 
 # TODO: analyze which words can follow other words
 # Your code here
+words = words.split(" ")
+# wordsRange = re.findall('([A-Z][.?!]+)', words)
+# print(wordsRange)
+
+
+def build_index(words):
+    index = {}
+    star_words = []
+    # loop through the split word list
+    # add the next word to the word list of that index
+    for idx in range(len(words) - 1):
+        # check if the word is already in index
+        if words[idx] in index:
+            index[words[idx]].append(words[idx+1])
+        # if not, add the key and append the next word
+        else:
+            index[words[idx]] = []
+            index[words[idx]].append(words[idx+1])
+
+    return index
 
 
 # TODO: construct 5 random sentences
 # Your code here
-
+idx = build_index(words)
+# print(idx)
+s = random.choice(words)
+print(" ".join(idx[s]), end=" ")
